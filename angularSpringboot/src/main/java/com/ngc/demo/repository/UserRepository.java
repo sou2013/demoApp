b917dee8e2095511ffd320f2f6efed798f37289f
nameabc <a@test.com>
2020-09-23 06:09:30 -0700

    delete files

--- angularSpringboot/src/main/java/com/ngc/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ngc.demo.repository;
-
-import com.ngc.demo.entity.Users;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@Repository
-public interface UserRepository extends CrudRepository<Users, Long>{}
--- angularSpringboot/src/main/java/com/ngc/ess/scmp/repository/TIRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ngc.ess.scmp.repository;
-
-import com.ngc.ess.scmp.entity.TerminalImage;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@Repository
-public interface TIRepository extends CrudRepository<TerminalImage, Long>{}
